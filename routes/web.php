f8f6ec8570d3dd7afb708d461fe46eb971e76ffd
AmongMember <bumeng873@gmail.com>
2024-01-19 18:07:00 +0900

    postsフォルダを作成しindex.blade.phpを移動

--- routes/web.php
+++ routes/web.php
@@ -15,8 +15,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/', function() {
+    return view('posts.index');
 });
 
 Route::get('/dashboard', function () {
