b61ed4a6bb9ecc92d482dadf796f6cb69f99933e
AmongMember <bumeng873@gmail.com>
2024-02-01 17:10:48 +0900

    createでの内容がstoreができないエラーを解決

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -8,6 +8,7 @@
     </head>
     <body>
         <h1>Bud</h1>
+        <a href='/posts/create'>create</a>
         <div class='posts'>
             @foreach ($posts as $post)
                 <div class='post'>
--- routes/web.php
+++ routes/web.php
@@ -18,9 +18,15 @@ use Illuminate\Support\Facades\Route;
 // Route::get('/', function () {
 //     return view('welcome');
 // });
-Route::get('/', [PostController::class, 'index']);   
+Route::get('/', [PostController::class, 'index']);  
+
+Route::post('/posts',[PostController::class,'store']);
+
+Route::get('/posts/create',[PostController::class,'create']);
 
 Route::get('/posts/{post}',[PostController::class,'show']);
+
+
 
 Route::get('/dashboard', function () {
     return view('dashboard');
