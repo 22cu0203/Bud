689e6d1ad35d503f83efe7608db475cccbc0f83b
AmongMember <bumeng873@gmail.com>
2024-02-06 21:34:34 +0900

    投稿を編集できるようにする処理を追加

--- routes/web.php
+++ routes/web.php
@@ -26,6 +26,10 @@ Route::get('/posts/create',[PostController::class,'create']);
 
 Route::get('/posts/{post}',[PostController::class,'show']);
 
+Route::get('/posts/{post}/edit', [PostController::class, 'edit']);
+
+Route::put('/posts/{post}', [PostController::class, 'update']);
+
 
 
 Route::get('/dashboard', function () {
