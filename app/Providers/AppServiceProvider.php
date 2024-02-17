50bd32b281985557e74abae7edde147ae163bc83
AmongMember <bumeng873@gmail.com>
2024-02-17 16:10:00 +0900

    ログイン機能の自訴王

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,10 +24,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        Paginator::useBootstrap(); //動画
+        //Paginator::useBootstrap(); //動画
         
         // Paginator::useBootstrapFive();    　公式ドキュメント
-       //または Paginator::useBootstrapFour();　　   公式ドキュメント
+        //または Paginator::useBootstrapFour();　　   公式ドキュメント
         
         \URL::forceScheme('https'); //追加
         $this->app['request']->server->set('HTTPS','on');
--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -1,26 +1,31 @@
-<body>
-    <h1 class="title">編集画面</h1>
-    <div class="content">
-        <form action="/posts/{{ $post->id }}" method="POST">
-            @csrf
-            @method('PUT')
-            <div class='content__title'>
-                <h2>タイトル</h2>
-                <input type='text' name='post[title]' value="{{ $post->title }}">
-            </div>
-            <div class='content__body'>
-                <h2>本文</h2>
-                <input type='text' name='post[body]' value="{{ $post->body }}">
-            </div>
-            <div class="category">
-                <h2>Category</h2>
-                <select name="post[category_id]">
-                    @foreach($categories as $category)
-                        <option value="{{ $category->id }}">{{ $category->name }}</option>
-                    @endforeach
-                </select>
-            </div>
-            <input type="submit" value="保存">
-        </form>
-    </div>
-</body>
+<x-app-layout>
+    <x-slot name="header">
+        Bud
+    </x-slot>
+    <body>
+        <h1 class="title">編集画面</h1>
+        <div class="content">
+            <form action="/posts/{{ $post->id }}" method="POST">
+                @csrf
+                @method('PUT')
+                <div class='content__title'>
+                    <h2>タイトル</h2>
+                    <input type='text' name='post[title]' value="{{ $post->title }}">
+                </div>
+                <div class='content__body'>
+                    <h2>本文</h2>
+                    <input type='text' name='post[body]' value="{{ $post->body }}">
+                </div>
+                <div class="category">
+                    <h2>Category</h2>
+                    <select name="post[category_id]">
+                        @foreach($categories as $category)
+                            <option value="{{ $category->id }}">{{ $category->name }}</option>
+                        @endforeach
+                    </select>
+                </div>
+                <input type="submit" value="保存">
+            </form>
+        </div>
+    </body>
+</x-app-layout>
